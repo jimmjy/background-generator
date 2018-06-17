0b27e25b49b60494dd5c540a78349333842ccbbc
James Finkelstein <jfinkelstein9@me.com>
2018-06-16 23:54:48 -0400

    updated js file

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
+//default variables
 var color1 = document.querySelector('.color1');
-var color2 = document.querySelector('.color1');
+var color2 = document.querySelector('.color2');
+var body = document.getElementById('background');
+var colorOutput = document.querySelector('h3');
+
+//functions
+
+//background setter
+function setBackground() {
+    body.style.background = "linear-gradient(to right, " + color1.value + " , " + color2.value + ")";
+    colorText();
+};
+
+//output current background value to screen
+function colorText() {
+    colorOutput.textContent = body.style.background;
+}
+
+//listen for changes to color picker 1
+color1.addEventListener('change', setBackground);
+
+//listen for changes to color picker 2
+color2.addEventListener('change', setBackground);
+
+//onload functions
+setBackground();
+colorText();
 
